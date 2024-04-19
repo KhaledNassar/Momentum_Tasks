ee3bd47e9c891913dd3aeb4cd1e08be3c62b5fd1
Khaled Nassar <160269258+KhaledNassar@users.noreply.github.com>
2024-04-19 14:16:56 +0200

    Create list_interface.h

---
+++ Data Structure/List Array_based Implementation/list_interface.h
@@ -0,0 +1,23 @@
+#ifndef _LIST_INTERFACE_H
+#define _LIST_INTERFACE_H
+
+#define ListEntry int
+#define MAXLIST 10
+
+typedef struct list{
+    ListEntry entry[MAXLIST];
+    int Size;
+}List;
+
+void List_Init(List *pl);
+int List_Empty(List *pl);
+int List_Full(List *pl);
+int List_Size(List *pl);
+void List_Destroy(List*);
+void List_Traverse(List *pl, void(*visit)(ListEntry));
+int List_Insert_Node(int position, ListEntry e, List *pl);
+void List_Delete_Node(int position, ListEntry *pe, List *pl);
+void List_Retrieve(int p, ListEntry *pe, List *pl);
+void List_Replace(int p, ListEntry e, List *pl);
+
+#endif // _LIST_INTERFACE_H
